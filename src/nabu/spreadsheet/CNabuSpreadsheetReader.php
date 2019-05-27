a2427e81b9bbe620ac4e3433ac865684766385cf
Rafael Gutiérrez <ragutimar@gmail.com>
2019-05-27 19:02:12 +0200

    Partial modif to accomplish new Data List Reader infrastructure.

--- src/nabu/spreadsheet/CNabuSpreadsheetReader.php
+++ src/nabu/spreadsheet/CNabuSpreadsheetReader.php
@@ -23,7 +23,7 @@ namespace nabu\spreadsheet;
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-use nabu\min\CNabuObject;
+use nabu\infrastructure\reader\CNabuAbstractDataListFileReader;
 
 use nabu\spreadsheet\data\CNabuSpreadsheetData;
 use nabu\spreadsheet\data\CNabuSpreadsheetDataRecord;
@@ -39,57 +39,27 @@ use PhpOffice\PhpSpreadsheet;
  * @version 0.0.2
  * @package \nabu\spreadsheet
  */
-class CNabuSpreadsheetReader extends CNabuObject
+class CNabuSpreadsheetReader extends CNabuAbstractDataListFileReader
 {
-    /** @var string|null $filename Full path filename of MS(R) Excel file. */
-    private $filename = null;
     /** @var PhpSpreadsheet|null $spreadsheet Native spreadsheet instance. */
     private $spreadsheet = null;
 
-    /**
-     * Creates the instance with the possibility to pass the file name of a MS(R) Excel file to open it.
-     * @param string|null $filename Filename to read.
-     */
-    public function __construct(string $filename = null)
+    protected function getValidMIMETypes(): array
     {
-        parent::__construct();
-
-        if (is_string($filename)) {
-            $this->loadFromFile($filename);
-        }
+        throw new \LogicException('Not implemented'); // TODO
     }
 
-    /**
-     * Validates a file name.
-     * @param string $filename Filename to read.
-     * @throws ENabuSpreadsheetUtilsException Throws an exception if something unexpected success opening the file.
-     */
-    private function validateFilename(string $filename): void
+    protected function customFileValidation(string $filename): bool
     {
-        if (strlen($filename) === 0 ||
-            ($this->filename = realpath($filename)) === false ||
-            mb_strlen($this->filename) === 0 ||
-            !file_exists($this->filename) ||
-            !is_file($this->filename)
-        ) {
-            $this->filename = null;
-            throw new ENabuSpreadsheetUtilsException(
-                ENabuSpreadsheetUtilsException::ERROR_INVALID_FILE_NAME_OR_PATH,
-                array($filename)
-            );
-        }
+        return true;
     }
 
-    /**
-     * Loads a MS(R) Excel file.
-     * @param string $filename Filename to read.
-     * @throws ENabuSpreadsheetUtilsException Throws an exception if something unexpected success opening the file.
-     */
-    public function loadFromFile(string $filename): void
+    protected function openSourceFile(string $filename): bool
     {
-        $this->validateFilename($filename);
         $this->spreadsheet = IOFactory::load($this->filename);
         $this->setActiveSheetIndex(0);
+
+        return true;
     }
 
     /**
@@ -154,99 +124,5 @@ class CNabuSpreadsheetReader extends CNabuObject
         return new CNabuSpreadsheetData($index_field);
     }
 
-    /**
-     * Calculates the matrix to translate from initial column naming (A, B, C...) to final translation fields.
-     * This method grants that columns could be unordered and extra columns interlaced between required columns.
-     * @param array $translation_fields Associative array to translate fields from the first line column name
-     * of the datasheet to the well formed name in the result data instance.
-     * @param array $column_names Original column names found in the first line of the datasheet.
-     * @param bool $canonize If set to true, canonizes column names to remove unecessary lead paddings and special chars.
-     * @return array|null Returns an array with translation of columns.
-     */
-    private function calculateColumnNameTranslations(
-        array $translation_fields, array $column_names, bool $canonize = false
-    ): ?array {
-        $keys = array_values($column_names);
-        $values = array_keys($column_names);
 
-        if ($canonize) {
-            array_walk($keys, function(&$value) {
-                $value = mb_strtolower(preg_replace('/_+$/', '', preg_replace('/^_+/', '', preg_replace('/[\s\.\(\)]+/', '_', $value))));
-                $value = str_replace(SPSUTILS_VOCALS, SPSUTILS_CANONICAL, $value);
-            });
-        }
-
-        $new_keys = array_intersect_key($translation_fields, array_combine($keys, $values));
-        $new_values = array_intersect_key(array_combine($keys, $values), $translation_fields);
-
-        $translated_columns = array();
-        foreach ($new_keys as $key => $value) {
-            $translated_columns[$value] = $new_values[$key];
-        }
-
-        return $translated_columns;
-    }
-
-    /**
-     * Check if all mandatory fields are present in columns.
-     * @param array $fields Fields found in columns.
-     * @param array $required_fields List of mandatory field names.
-     * @throws ENabuSpreadsheetUtilsException Throws an exception if some of mandatory fields are not present.
-     */
-    private function checkMandatoryFields(array $fields, array $required_fields): void
-    {
-        if (!(count($required_fields) === count(array_intersect(array_keys($fields), $required_fields)))) {
-            $missed_columns = array_diff($required_fields, array_keys($fields));
-            error_log(print_r($missed_columns, true));
-            throw new ENabuSpreadsheetUtilsException(
-                ENabuSpreadsheetUtilsException::ERROR_MANDATORY_COLUMNS_NOT_PRESENT,
-                array(
-                    implode(', ', $missed_columns)
-                )
-            );
-        }
-    }
-
-    /**
-     * Maps data in a @see { CNabuSpreadsheetData } instance and returns it.
-     * @param array $datasheet Source datasheet.
-     * @param array $map_fields Map columns correspondence to map data.
-     * @param array $required_fields List of mandatory field names.
-     * @param string|null $index_field If setted, the list is indexed using values in this field.
-     * @param int $offset Offset of first line of data to map. Default value is 1 but the Datasheet array
-     * of PhpSpreasheet is option base 1.
-     * @return CNabuSpreadsheetData Returns the instance of @see { CNabuSpreadsheetData } with translated data.
-     * @throws ENabuSpreadsheetUtilsException Throws an exception if something fails.
-     */
-    private function mapData(
-        array $datasheet, array $map_fields, array $required_fields, ?string $index_field = null, int $offset = 1
-    ): CNabuSpreadsheetData {
-        $resultset = $this->createDataInstance($index_field);
-
-        if (($l = count($datasheet)) >= $offset) {
-            for ($i = $offset; $i <= $l; $i++) {
-                if (array_key_exists($i, $datasheet)) {
-                    $source = $datasheet[$i];
-                    $reg = array();
-                    foreach ($map_fields as $key => $pos) {
-                        $reg[$key] = $source[$pos];
-                    }
-                    if (count($missed_fields = array_diff(array_values($required_fields), array_keys($reg))) > 0) {
-                        throw new ENabuSpreadsheetUtilsException(
-                            ENabuSpreadsheetUtilsException::ERROR_MANDATORY_COLUMNS_NOT_PRESENT_IN_LINE,
-                            array(implode(', ', $missed_fields), $i)
-                        );
-                    }
-                    $record = new CNabuSpreadsheetDataRecord($reg);
-                    if (is_null($index_field)) {
-                        $resultset->addItem($record, $i - $offset);
-                    } else {
-                        $resultset->addItem($record);
-                    }
-                }
-            }
-        }
-
-        return $resultset;
-    }
 }
